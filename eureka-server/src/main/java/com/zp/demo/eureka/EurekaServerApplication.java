0d59aee3ba9157fcf737fe8812ba477494d75597
zp <zp@zving.com>
2018-07-19 16:16:22 +0800

    添加注释

--- eureka-server/src/main/java/com/zp/demo/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/zp/demo/eureka/EurekaServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author zherop@163.com
+ * @date 2018年7月19日
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
