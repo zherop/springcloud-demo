e743f9a62437aa253c244d98202bb33b1ed29f3b
zp <zp@zving.com>
2018-07-20 09:55:09 +0800

    修改hello-service：添加路由/say

--- hello-service/src/main/java/com/zp/demo/hello/controller/HelloController.java
+++ hello-service/src/main/java/com/zp/demo/hello/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.zp.demo.hello.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ public class HelloController {
 	@Value("${spring.application.name}")
 	private String serverName;
 
+	@GetMapping("/say")
 	public String say() {
 		return "say hello from " + serverName;
 	}
