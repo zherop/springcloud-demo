d3740ccd8dfe818c5ed18c60c996ff33f4096b2d
zp <zp@zving.com>
2018-08-01 14:15:45 +0800

    zuul-service:添加AuthFilter

---
+++ zuul-service/src/main/java/com/zp/demo/zuul/filter/AuthFilter.java
@@ -0,0 +1,54 @@
+package com.zp.demo.zuul.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class AuthFilter extends ZuulFilter {
+	private static Logger log = LoggerFactory.getLogger(AuthFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		log.info(String.format("%s %s", request.getMethod(), request.getRequestURL().toString()));
+		Object accessToken = request.getParameter("token");
+		if (accessToken == null) {
+			log.warn("token is empty");
+			ctx.setSendZuulResponse(false);
+			ctx.setResponseStatusCode(401);
+			try {
+				ctx.getResponse().getWriter().write("token is empty");
+			} catch (Exception e) {
+			}
+
+			return null;
+		}
+		log.info("ok");
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return FilterConstants.PRE_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+}
